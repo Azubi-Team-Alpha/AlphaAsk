006ffca5f6720b4d986be960fa4c6960724c2e83
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-07-29 15:26:34 +0000

    fix(terraform): add S3 public read bucket policy for frontend static website hosting

--- infra/terraform/s3_cloudfront.tf
+++ infra/terraform/s3_cloudfront.tf
@@ -26,6 +26,24 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
   }
 }
 
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket     = aws_s3_bucket.frontend.id
+  depends_on = [aws_s3_bucket_public_access_block.frontend_public]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
+
 # Optional CloudFront Distribution for Frontend CDN & SSL
 resource "aws_cloudfront_distribution" "frontend_cdn" {
   count = var.enable_cloudfront ? 1 : 0
