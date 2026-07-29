40cb63f66c0ff3cbf32c53b58bec5494778a94ca
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-07-29 13:21:44 +0000

    fix(terraform): add force_delete to ECR repository resource

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.app_name}-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -10,3 +11,4 @@ resource "aws_ecr_repository" "backend" {
     Name = "${var.app_name}-ecr-repo"
   }
 }
+
